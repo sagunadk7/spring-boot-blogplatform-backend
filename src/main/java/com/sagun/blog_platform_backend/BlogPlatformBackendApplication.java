edfe81baa7a0ab22be31fa34588a24c8769f5e47
sagunadk7 <sagunadhikari100@gmail.com>
2026-09-12 15:21:47 +0545

    First commit

---
+++ src/main/java/com/sagun/blog_platform_backend/BlogPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sagun.blog_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagun/blog_platform_backend/BlogPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagun.blog_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
